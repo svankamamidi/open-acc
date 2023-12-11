e03a22d9c8af22cf7430033d3dc1fc1c2e96fe3e
Sudhakar Vankamamidi <sudhakarvm@yahoo.co.in>
2023-12-12 02:46:40 +0530

    Service worker

---
+++ sw.js
@@ -0,0 +1,41 @@
+// Files to cache
+const cacheName = 'aac2'
+const appShellFiles = [
+  '/open-aac/',
+  '/open-aac/index.html',
+  '/open-aac/icons/aac.png',
+  'https://code.jquery.com/ui/1.13.0/themes/base/jquery-ui.css',
+  'https://code.jquery.com/jquery-3.6.0.js',
+  'https://code.jquery.com/ui/1.13.0/jquery-ui.js'
+];
+
+// Installing Service Worker
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+  e.waitUntil((async () => {
+    const cache = await caches.open(cacheName);
+    console.log('[Service Worker] Caching all: app shell and content');
+    await cache.addAll(appShellFiles);
+  })());
+});
+
+// Fetching content using Service Worker
+self.addEventListener('fetch', (e) => {
+    // Cache http and https only, skip unsupported chrome-extension:// and file://...
+    if (!(
+       e.request.url.startsWith('http:') || e.request.url.startsWith('https:')
+    )) {
+        return; 
+    }
+
+  e.respondWith((async () => {
+    const r = await caches.match(e.request);
+    console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+    if (r) return r;
+    const response = await fetch(e.request);
+    const cache = await caches.open(cacheName);
+    console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+    cache.put(e.request, response.clone());
+    return response;
+  })());
+});
