ba4e5966a82cc535f02af958c7c5ccd0ffd230a8
Sudhakar Vankamamidi <sudhakarvm@yahoo.co.in>
2024-06-06 23:30:21 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 // Files to cache
-const cacheName = 'aac5.3'
+const cacheName = 'aac5.4'
 const appShellFiles = [
   '/open-aac/',
-  //'/open-aac/index.html',
+  '/open-aac/index.html',
   '/open-aac/icons/aac.png',
   'https://code.jquery.com/ui/1.13.0/themes/base/jquery-ui.css',
   'https://code.jquery.com/jquery-3.6.0.js',
